97596a554a5258c248d1480c8beb3933668a513d
Ludovic CH <ludovic.chapelet@etu.univ-smb.fr>
2017-09-27 19:14:43 +0200

    Setup vs

---
+++ src/Motor/Motor/Motor.cpp
@@ -0,0 +1,11 @@
+/*
+ Name:		Motor.cpp
+ Created:	27/09/2017 19:00:27
+ Author:	Ludovic
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "Motor.h"
+
+
+
---
+++ src/Motor/Motor/Motor.h
@@ -0,0 +1,19 @@
+/*
+ Name:		Motor.h
+ Created:	27/09/2017 19:00:27
+ Author:	Ludovic
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _Motor_h
+#define _Motor_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
